1526a7d6e16d8acbf65942366b1d17abf1f991e1
Josh Gulledge <jgulledge19@gmail.com>
2023-03-11 07:08:59 -0700

    Added input feild and styling

--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -1,9 +1,15 @@
-const InputFeild = () => {
-    return (
-        <div>
+import "./styles.css"
 
-        </div>
-    )
+interface Props{
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+
+const InputFeild = ({todo, setTodo}: Props) => {
+    return <form className="input">
+        <input type="input" placeholder="Enter a task" className="input_box"/>
+        <button className="input_submit" type="submit">Go</button>
+    </form>
 }
 
 export default InputFeild
